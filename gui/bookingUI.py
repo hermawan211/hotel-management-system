a63c42e9d64638e27b95529ecbdf19d53a3142ee
hermawan211 <aliangxie12@gmail.com>
2024-05-06 14:23:14 +0700

    adding details on booking

--- gui/bookingUI.py
+++ gui/bookingUI.py
@@ -2,6 +2,10 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from tkinter.ttk import *
+from datetime import date
+
+today = date.today()
+today = today.strftime("%d-%m-%Y")
 
 class BookingView(tk.Frame):
     def __init__(self, parent, room):
@@ -20,12 +24,25 @@ class BookingView(tk.Frame):
                                   font=("Helvetica", 14, "bold"))
         booking_label.grid(row=0, column=0, columnspan=3)
 
+
+        # name
         name_label = ttk.Label(booking_frame, text="Name",
                                   font=("Helvetica", 12,))
         name_label.grid(row=1, column=0,)
 
+        # Phone
+        phone_label = ttk.Label(booking_frame, text="Phone",
+                                  font=("Helvetica", 12,))
+        phone_label.grid(row=2, column=0,)
+
+        # date
+        date_label = ttk.Label(booking_frame, text=today,
+                               font=("Helvetica", 12, 'bold'))
+        date_label.grid(row=0, column=4, sticky='e', padx=(80, 0))
 
         # room number
         room_label = ttk.Label(booking_frame, text=room,
                                   font=("Helvetica", 12,))
         room_label.grid(row=1, column=3,)
+
+        
